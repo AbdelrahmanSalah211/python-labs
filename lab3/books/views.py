8767f0d87c65a0f6a58a153e7993317f31a989c0
Abdelrahman Salah <abdelrahmansalah211@gmail.com>
2025-07-03 00:36:22 +0300

    screenshots and fix

--- lab3/books/views.py
+++ lab3/books/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.template.response import TemplateResponse
 from django.http import Http404
+from django.middleware.csrf import get_token
+from django.utils.safestring import mark_safe
 
 BOOKS = [
     {'id': 1, 'title': 'The Great Gatsby', 'author': 'F. Scott Fitzgerald', 'description': 'A classic novel set in the Jazz Age.'},
@@ -34,7 +36,14 @@ def book_create(request):
             'description': request.POST['description'],
         })
         return redirect('book_list')
-    return TemplateResponse(request, 'books/book_form.html', {'action': 'Add'})
+    
+    csrf_token = get_token(request)
+    csrf_input = mark_safe(f'<input type="hidden" name="csrfmiddlewaretoken" value="{csrf_token}">')
+    return TemplateResponse(request, 'books/book_form.html', {
+        'action': 'Add',
+        'csrf_input': csrf_input,
+        'book': {}
+    })
 
 
 def book_update(request, book_id):
@@ -46,7 +55,14 @@ def book_update(request, book_id):
         book['author'] = request.POST['author']
         book['description'] = request.POST['description']
         return redirect('book_list')
-    return TemplateResponse(request, 'books/book_form.html', {'book': book, 'action': 'Edit'})
+    
+    csrf_token = get_token(request)
+    csrf_input = mark_safe(f'<input type="hidden" name="csrfmiddlewaretoken" value="{csrf_token}">')
+    return TemplateResponse(request, 'books/book_form.html', {
+        'book': book,
+        'action': 'Edit',
+        'csrf_input': csrf_input
+    })
 
 
 def book_delete(request, book_id):
